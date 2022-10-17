9729baff01c42e8b17c6671f15f15f707b1b3e79
heikkipatsi <c0pahe04@students.oamk.fi>
2022-10-17 14:37:59 +0300

    Toimiva peli

---
+++ server/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: 'localhost',
+        user: 'root',
+        database: 'todo',
+        password: 'root',
+        port: 8889
+    }
+}
+module.exports = config
